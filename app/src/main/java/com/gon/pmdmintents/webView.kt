7617f70a2a7b9634c9dd561fc1a412014df5eb06
dam2 <ggarciaalvarez@danielcastelao.org>
2019-12-12 12:40:52 +0100

    Camara

--- app/src/main/java/com/gon/pmdmintents/webView.kt
+++ app/src/main/java/com/gon/pmdmintents/webView.kt
@@ -2,11 +2,18 @@ package com.gon.pmdmintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_web_view.*
+import org.jetbrains.anko.browse
 
 class webView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+        busqueda.setOnClickListener {
+            browse("https://steamuserimages-a.akamaihd.net/ugc/475500450718487368/DC2A6E4A68F1780F0B657426D075D1F22768A10B/")
+        }
+
     }
 }
